4feb20a326ffd219fed794dee6359e0306e2bd7f
FooKing <skaged@live.co.uk>
2022-11-11 22:56:19 +0000

    MoreInputings

--- Assets/Scripts/StickmanBalance.cs
+++ Assets/Scripts/StickmanBalance.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net.NetworkInformation;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class StickmanBalance : MonoBehaviour
 {
     public float restingAngle = 0f;
-    public float force = 750f;
+    public float force = 50f;
     public bool RestingUpdate = true;
+    
+    
+    // Movement Variables
+    public float WalkSpeed = 50;
+    public float JumpPower = 50;
+    public bool canJump = true;
 
     public Bones[] bonesArray;
+    
+    [SerializeField] public Rigidbody2D mainBody;
 
 
     // Class to hold all references to body parts.
@@ -28,7 +37,7 @@ public class StickmanBalance : MonoBehaviour
 
     private void Awake()
     {
-        
+
         //Disable collision with own body parts
         Collider2D [] colliders = GetComponentsInChildren<Collider2D>();
         for (int i = 0; i < colliders.Length; i++)
@@ -41,18 +50,29 @@ public class StickmanBalance : MonoBehaviour
             }
         }
     }
-
+    
 
     private void Update()
     {
-        if (Input.GetAxis("Horizontal") < 50f);
+        if (Input.GetAxisRaw("Horizontal") >= 1)
         {
-            Debug.Log(Input.GetAxis("Horizontal"));
-            Rigidbody2D pelvis = bonesArray[1].muscle;
-            pelvis.AddForce(bonesArray[1].muscle.transform.right * (Input.GetAxis("Horizontal") * 20));
+            MoveRight();
+
         }
-        
+
+        if (Input.GetAxisRaw("Horizontal") <= -1)
+        {
+            MoveLeft();
+
+        }
+
+        if (Input.GetButton("Jump") && canJump)
+        {
+            StartJump();
+        }
+
     }
+
     private void FixedUpdate()
     {
         // Add rotational force to achieve characters balance.
@@ -61,7 +81,35 @@ public class StickmanBalance : MonoBehaviour
             currentBone.muscle.MoveRotation(Mathf.LerpAngle(currentBone.muscle.rotation, currentBone.MuscleAngle, currentBone.MuscleForce * Time.deltaTime));
 
         }
+
+        
     }
 
     
+    
+    public void MoveRight()
+    {
+        mainBody.velocity = new Vector2(10 * (WalkSpeed * Time.deltaTime), mainBody.velocity.y);
+    }
+    
+    public void MoveLeft()
+    {
+        mainBody.velocity = new Vector2(10 * (WalkSpeed * Time.deltaTime) * -1, mainBody.velocity.y);
+    }
+
+    private void StartJump()
+    {
+        canJump = false;
+        mainBody.velocity = new Vector2(mainBody.velocity.x, JumpPower);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("CollisionHit" + col.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("TriggerHit" + col.gameObject.name);
+    }
 }
